994c297521975def3095d3a072b32862bf4cd9f5
abraham <muzhahov@bk.ru>
2022-12-22 02:06:43 +0300

    commit

---
+++ app.py
@@ -0,0 +1,47 @@
+import requests
+from flask import Flask, render_template, request, redirect
+import psycopg2
+
+app = Flask(__name__)
+
+conn = psycopg2.connect(database="service_db", user="postgres", password="magamet123", host="localhost", port="5432")
+
+cursor = conn.cursor()
+
+
+@app.route('/login/', methods=['GET'])
+def index():
+    return render_template('login.html')
+
+@app.route('/login/', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        if request.form.get("login"):
+            try:
+                username = request.form.get('username')
+                password = request.form.get('password')
+                cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+                records = list(cursor.fetchall())
+                return render_template('account.html', full_name=records[0][1], login=records[0][2], password=records[0][3])
+            except Exception:
+                return render_template("error.html")
+        elif request.form.get("registration"):
+            return redirect("/registration/")
+    return render_template('login.html')
+
+@app.route('/registration/', methods=['POST', 'GET'])
+def registration():
+    if request.method == 'POST':
+        try:
+            name = request.form.get("name")
+            login = request.form.get("login")
+            password = request.form.get("password")
+            cursor.execute("INSERT INTO service.users(full_name, login, password) VALUES(%s, %s, %s);", (str(name), str(login), str(password)))
+            conn.commit()
+            return redirect("/login/")
+        except Exception:
+            return render_template("error.html")
+    return render_template("registration.html")
+
+if __name__ == "__main__":
+    app.run()
